a1d12b7a6f0d141f934e54dfc4f657b7a391b691
Najah <najahsr2@gmail.com>
2020-05-02 22:59:36 +0300

    Initial Commit

---
+++ HW1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HW1
+//
+//  Created by najah  on 5/2/20.
+//  Copyright © 2020 najah . All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
